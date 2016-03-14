59a490f93b60832690d8b15e86893afe24072715
apipopazfar <apipoazfar@gmail.com>
2016-03-14 17:15:59 +0800

    add some ionicjs

--- mypp/www/js/controllers.js
+++ mypp/www/js/controllers.js
@@ -1,8 +1,51 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $ionicActionSheet) {
+.controller('DashCtrl', function($scope, $ionicLoading, $timeout) {
+   $scope.showLoading = function() {
+      $ionicLoading.show({
+         template: 'Wait 3 second...'
+      });
+      $timeout(function() { 
+          $ionicLoading.hide(); //just change the function names
+        }, 3000); //use timeout function with 3 second delay (same as backdrop)
+   };
+})
 
-$scope.triggerActionSheet = function() {
+.controller('ChatsCtrl', function($scope, Chats, $ionicBackdrop, $timeout) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  $scope.showBackdrop = function() {
+        $ionicBackdrop.retain();
+        $timeout(function() { 
+          $ionicBackdrop.release();
+        }, 3000);
+  };
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+  $scope.chat= [
+   {id: 1},
+   {id: 2},
+   {id: 3},
+   {id: 4}
+];
+
+$scope.moveItem = function(chat, fromIndex, toIndex) {
+   $scope.chats.splice(fromIndex, 1);
+   $scope.chats.splice(toIndex, 0, chat);
+};
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats, $ionicActionSheet) {
+  $scope.chat = Chats.get($stateParams.chatId);
+  $scope.triggerActionSheet = function() {
 
         // Show the action sheet
         var showActionSheet = $ionicActionSheet.show({
@@ -31,33 +74,107 @@ $scope.triggerActionSheet = function() {
     };
 })
 
-.controller('ChatsCtrl', function($scope, Chats, $ionicBackdrop, $timeout) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  $scope.showBackdrop = function() {
-        $ionicBackdrop.retain();
-        $timeout(function() { 
-          $ionicBackdrop.release();
-        }, 3000);
-  };
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function($scope, $ionicPopup) {
   $scope.settings = {
     enableFriends: true
   };
-});
+  $scope.showPopup = function() {
+      $scope.data = {}
+    
+      // Custom popup
+      var myPopup = $ionicPopup.show({
+         template: '<input type = "text" ng-model = "data.model">',
+         title: 'Simple Chat',
+         subTitle: 'Newsletter',
+         scope: $scope,
+      
+         buttons: [
+            { text: 'Cancel' }, {
+               text: '<b>Subscribe</b>',
+               type: 'button-positive',
+                  onTap: function(e) {
+            
+                     if (!$scope.data.model) {
+                        //don't allow the user to close unless he enters model...
+                           e.preventDefault();
+                     } else {
+                        return $scope.data.model;
+                     }
+                  }
+            }
+         ]
+      });
+
+      myPopup.then(function(res) {
+         console.log('Tapped!', res);
+      });    
+   };
+  // When button is clicked, the popup will be shown...
+   $scope.showConfirm = function() {
+  
+      var confirmPopup = $ionicPopup.confirm({
+         title: 'Exit',
+         template: 'Are you sure?'
+      });
+
+      confirmPopup.then(function(res) {
+         if(res) {
+            console.log('Sure!');
+         } else {
+            console.log('Not sure!');
+         }
+      });
+    
+   };
+   $scope.showAlert = function() {
+  
+      var alertPopup = $ionicPopup.alert({
+         title: 'Error',
+         template: 'Something was missing'
+      });
+
+      alertPopup.then(function(res) {
+         // Custom functionality....
+      });
+   };
+   $scope.showPrompt = function() {
+  
+      var promptPopup = $ionicPopup.prompt({
+         title: 'Title',
+         template: 'Template text',
+         inputType: 'text',
+         inputPlaceholder: 'Placeholder'
+      });
+        
+      promptPopup.then(function(res) {
+         console.log(res);
+      });
+    
+   };
+})
+  .controller('TestCtrl', function($scope, $ionicScrollDelegate, $ionicSlideBoxDelegate) {
+   $scope.items = [];
+
+   $scope.noMoreItemsAvailable = false;
+  
+   $scope.loadMore = function() {
+
+      $scope.items.push({ id: $scope.items.length}); 
+
+      if ($scope.items.length == 30) {
+         $scope.noMoreItemsAvailable = true;
+      }
+
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+   };
+   $scope.scrollTop = function() {
+      $ionicScrollDelegate.scrollTop();
+   };
+    $scope.nextSlide = function() {
+      $ionicSlideBoxDelegate.next();
+   }
+   $scope.previousSlide = function() {
+      $ionicSlideBoxDelegate.previous();
+   }
+})
+;
